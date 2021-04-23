b4953e4c80f763d721c44375637c083965190b53
mfsun00 <365devops@gmail.com>
2021-04-23 17:37:54 +0900

    init

---
+++ src/main/kotlin/com/example/vcpif/VcpifApplication.kt
@@ -0,0 +1,11 @@
+package com.example.vcpif
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VcpifApplication
+
+fun main(args: Array<String>) {
+    runApplication<VcpifApplication>(*args)
+}
---
+++ src/main/kotlin/com/example/vcpif/config/KafkaConfig.kt
@@ -0,0 +1,4 @@
+package com.example.vcpif.config
+
+class KafkaConfig {
+}
